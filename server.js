6e66a2ab6f23504a3befde610257d2dbca5b0412
dhanushpatel <dhanush.p@asambhav.in>
2021-09-01 23:50:38 +0530

    updated build and server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const expressValidator = require("express-validator")
 require("dotenv").config()
+const path = require('path');
 
 const userRoutes = require("./routes/user")
 const authRoutes = require("./routes/auth")
@@ -23,7 +24,7 @@ mongoose
         useUnifiedTopology: true,
     })
     .then(() => console.log("DB Connected"))
-    .catch((error) => {mongoose.connect(process.env.DATABASE, {useNewUrlParser: true,useUnifiedTopology:true})})
+    .catch(err => console.log(err));
 
 mongoose.connection.on("error", (err) => {
     console.log(`DB connection error: ${err.message}`)
@@ -41,6 +42,10 @@ app.use("/api", categoryRoutes)
 app.use("/api", expenseRoutes)
 app.use("/api", incomeRoutes)
 
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8000
 
@@ -48,6 +53,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
 }
 
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 })
